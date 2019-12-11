48639b7d49069798b94bb29f11906588e998db8e
Michael Wagner <wagner.mchl@googlemail.com>
2019-12-12 00:25:12 +0800

    added: scoped environments

--- src/main/java/com/github/micutio/jynk/interpreter/Interpreter.java
+++ src/main/java/com/github/micutio/jynk/interpreter/Interpreter.java
@@ -130,6 +130,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
 
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+
+        environment.assign(expr.name, value);
+        return value;
+    }
+
     private boolean isEqual(Object a, Object b) {
         // nil is only equal to nil
         if (a == null && b == null)
--- src/main/java/com/github/micutio/jynk/parsing/Environment.java
+++ src/main/java/com/github/micutio/jynk/parsing/Environment.java
@@ -8,7 +8,17 @@ import java.util.Map;
 
 public class Environment {
 
+    final Environment enclosing;
+
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     public void define(String name, Object value) {
         values.put(name, value);
@@ -19,6 +29,28 @@ public class Environment {
             return values.get(name.lexeme);
         }
 
+        if (enclosing != null) return enclosing.get(name);
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    /**
+     * Assign a variable of this or any enclosing scope the given value.
+     * Innermost scopes have priority to allow for shadowing.
+     * @param name Name of the variable
+     * @param value New value of the variable
+     */
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme)) {
+            values.put(name.lexeme, value);
+            return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
+        throw new RuntimeError(name, "Undefinde variable '" + name.lexeme + "'.");
     }
 }
