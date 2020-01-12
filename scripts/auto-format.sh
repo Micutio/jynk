c996e9f8e72f0bd9bae83570440d407bb19462f8
Michael Wagner <wagner.mchl@googlemail.com>
2020-01-12 12:25:52 +0800

    fixed: project source path

--- scripts/auto-format.sh
+++ scripts/auto-format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `find src/main/java/com/micutio/jynk/ -name "*.java" -type f`; do
+for i in `find src/main/java/com/github/micutio/jynk/ -name "*.java" -type f`; do
     echo "formatting" "$i"
     clang-format -i "$i"
 done
